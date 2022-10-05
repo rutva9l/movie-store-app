2481e5f39c6491249b44ca9adbcfd646b3a62496
rutva <rutva943@gmail.com>
2022-10-05 21:16:55 +0530

    minor

--- components/common/outline.jsx
+++ components/common/outline.jsx
@@ -5,7 +5,7 @@ const Outline = ({component}) => {
         <div className='h-[100vh] flex'>
             <Navbar />
             <div className="w-4/5 bg-neutral-200 h-full overflow-hidden">
-                <div className="px-8 py-6 bg-gray-100">sup</div>
+                <div className="px-8 py-6 bg-gray-100">Welcome!</div>
                 <div className="p-4">{component}</div>
             </div>
         </div>
